a7aa7669a0ec325faabd52f47ede428c79c4c68d
KevinBrinkman <108471693+KevinBrinkman@users.noreply.github.com>
2025-08-01 12:40:55 +0200

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,19 @@
+import GoogleProvider from "next-auth/providers/google";
+import type { AuthOptions } from "next-auth";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      return profile?.email?.endsWith("@netprofiler.nl") ?? false;
+    },
+    async redirect({ baseUrl }) {
+      return `${baseUrl}/dashboard`;
+    },
+  },
+};
